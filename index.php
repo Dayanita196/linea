230fef24d295d96c4005555b0e93049b8c999d8c
Dayanita196 <dayanahuaman347@gmail.com>
2024-10-06 20:13:02 -0500

    mi envio

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!--css de la  -->
+   <?php include 'partes/css.php'; ?>
+</head>
+<body class="px-3">
+    <!-- header de la pagina -->
+    <?php include 'partes/header.php'; ?>
+   
+     <div class="container mt-4 ">
+        <h1 class="h1">SOLUCIONES CREATIVAS</h1>
+        <p>sin importar cual sea tu negocio, nuestro equipo puede hacerlo desde cero.</p>
+        <div class="row">
+        <div class="col-sm-12 col-md-4">
+            <div class="card border-0">
+                <div class="card-body">
+                    <img class="w-100" src="resources/img/idea_01.PNG" alt="">
+                    <h5 class="h5 mt-2"><i class="bi bi-pencil-fill "></i>  DISEÑO</h5>
+                    <p class="pt-02"> creamos diseño segun tu necesidad</p>
+                </div>
+            </div>
+        </div>
+        
+
+        <div class="col-sm-12 col-md-4">
+            <div class="card border-0">
+                <div class="card-body">
+                    <img class="w-100" src="resources/img/idea_02.PNG" alt="">
+                    <h5 class="h5 mt-2"><i class="bi bi-flower3"></i>  DESARROLLO</h5>
+                    <p class="pt-02"> nuestros programadores te garantizan la perfeccion.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="col-sm-12 col-md-4">
+            <div class="card border-0">
+                <div class="card-body">
+                    <img class="w-100" src="resources/img/idea_03.PNG" alt="">
+                    <h5 class="h5 mt-2"><i class="bi bi-file-plus-fill"></i>  EDICION DE LIBROS</h5>
+                    <p class="pt-02"> Que el producto final llegue a tus clientes</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    
+    <?php include 'partes/footer.php'; ?>
+</body>
+</html>
